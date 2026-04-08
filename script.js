ed72c64b64dba32eb91e0b5253992947f7652c6d
vagneraugustodasilvaalmeida-ctrl <vagneraugustodasilvaalmeida@gmail.com>
2026-04-08 15:39:34 -0300

    atualizado 2026!!!! 🔥🔥🔥🔥🔥

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const botao = document.getElementById('SeuBotao');
+
+    botao.addEventListener('click', () => {
+        
+        alert(' Vai tomando!!');
+        
+        
+        console.log('Interação realizada com sucesso!');
+    });
+});
